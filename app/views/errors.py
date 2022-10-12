73206397b597026185f95e90696b7d33652bcd40
KHAGE <khazhanyants@mail.com>
2022-10-12 11:01:26 +0300

    added errorhandler 405

--- app/views/errors.py
+++ app/views/errors.py
@@ -1,13 +1,20 @@
+from typing import Tuple
+
 from flask import Blueprint, render_template
 
 app = Blueprint('errors_bp', __name__, template_folder='templates')
 
 
 @app.app_errorhandler(404)  # обработчик ошибки 404
-def page_not_found(e):
+def page_not_found(e) -> Tuple[str, int]:
     return render_template('404.html', error=e), 404
 
 
+@app.app_errorhandler(405)  # обработчик ошибки 404
+def page_not_found(e) -> Tuple[str, int]:
+    return render_template('500.html', error=e), 405
+
+
 @app.app_errorhandler(500)  # обработчик ошибки 500
-def server_error(e):
+def server_error(e) -> Tuple[str, int]:
     return render_template('500.html', error=e), 500
